0d8ef95a959730c6b1d72c477d963edf0fd1a8b5
Malar <anu1983in@gmail.com>
2014-11-10 23:00:04 +0100

    ip

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   def restrict_admin_tool_access_by_ipaddress
     whitelist = ['5.175.83.30' ,'127.0.0.1','0.0.0.0','192.168.0.30']
     unless whitelist.include?(request.remote_ip)
+      p request.remote_ip
      # redirect_to controller: 'produits', action: 'display'
     end
   end
