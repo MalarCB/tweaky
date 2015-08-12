d8d8f286e4a01295ec9011a646ea00dc918c0dab
Malarvizhy SOUPRAMANIEN <malarvizhy.soupramanien@careerbuilder.com>
2015-08-12 17:34:42 +0200

    access allow origin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  after_filter :set_access_control_headers
 
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Request-Method'] = '*'
+  end
+  
   def restrict_admin_tool_access_by_ipaddress
     whitelist = ['5.175.83.30' ,'127.0.0.1','0.0.0.0','192.168.0.30']
     unless whitelist.include?(request.remote_ip)
